03679d4c5ef75a2a220237ac4469415f3915695a
Rashed97 <rashedabdeltawab@gmail.com>
2015-03-30 23:03:21 +0100

    SystemUI: Statusbar: Add HSPA+ icons
    Forward! Forward port H+ icon in the status bar when connected to HSPA+
    Uses new vector drawables by blunden
    Migrated some code due to this split up: https://github.com/android/platform_frameworks_base/commit/da68f596282e60bee832dff07cc96bf64bd15939
    
    adapted for m by KreAch3R
    
    PS1: Initial commit
    PS2: New vectors (thanks blunden)
    PS3: Cleanup
    PS4: Use correct indentation (oops)
    PS5: Show correct icon in status bar (thanks mikeNG)
    PS6: Remove leftover Holo icons (thanks mikeNG)
    PS7: Rebase to depend on Holo icon removal (thanks mikeNG)
    PS8: Update commit message
    
    Ported to 5.1 (altaf-mahdi)
    
    Change-Id: Ia745c524d5e2530f50f12f961ea8533a4c8f5842
    
    Conflicts:
    	packages/SystemUI/res/values/custom_arrays.xml
    	packages/SystemUI/res/values/custom_config.xml
    	packages/SystemUI/res/values/custom_strings.xml
    
    Conflicts:
    	packages/SystemUI/res/values/icarus_strings.xml

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkControllerImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkControllerImpl.java
@@ -785,6 +785,7 @@ public class NetworkControllerImpl extends BroadcastReceiver
                             datatype.equals("e") ? TelephonyIcons.E :
                             datatype.equals("g") ? TelephonyIcons.G :
                             datatype.equals("h") ? TelephonyIcons.H :
+                            datatype.equals("hp") ? TelephonyIcons.HP :
                             datatype.equals("lte") ? TelephonyIcons.LTE :
                             datatype.equals("lte+") ? TelephonyIcons.LTE_PLUS :
                             TelephonyIcons.UNKNOWN;
@@ -868,6 +869,7 @@ public class NetworkControllerImpl extends BroadcastReceiver
         boolean show4gForLte = false;
         boolean hideLtePlus = false;
         boolean hspaDataDistinguishable;
+	boolean hspapDataDistinguishable;
 
         static Config readConfig(Context context) {
             Config config = new Config();
@@ -880,6 +882,8 @@ public class NetworkControllerImpl extends BroadcastReceiver
             config.hspaDataDistinguishable =
                     res.getBoolean(R.bool.config_hspa_data_distinguishable);
             config.hideLtePlus = res.getBoolean(R.bool.config_hideLtePlus);
+            config.hspapDataDistinguishable =
+                    res.getBoolean(R.bool.config_hspap_data_distinguishable);
             return config;
         }
     }
