55a5e0daff692dbe489465f2f1809b6fee459898
Martin Brabham <optedoblivion@cyngn.com>
2015-10-23 21:37:12 -0400

    Policy: handle volume key event as wake key

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3756,6 +3756,12 @@ public final class Settings {
         public static final String STATUS_BAR_SHOW_CARRIER = "status_bar_show_carrier";
 
         /**
+         * Whether to wake the screen with the volume keys, the value is boolean.
+         * @hide
+         */
+        public static final String VOLUME_WAKE_SCREEN = "volume_wake_screen";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
