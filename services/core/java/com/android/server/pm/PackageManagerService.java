57d0c0dc30aad6e151c34b283f018c44b07f20df
dhacker29 <dhackerdvm@gmail.com>
2015-11-24 01:53:47 -0500

    Core: Use ro.velvet.version to signal mIsUpgrade
    M: We use a static fingerprint that is only changed when a new OEM build is released, so every flash shows Android is starting instead of upgrading.  This will fix that.
    
    N: even though we dont have the starting/upgrading screen on N yet, this is still needed to delete the correct caches, and grant/deny specific runtime permissions like a true oem update would do
    
    Updated for Nougat By: @BeansTown106

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -2394,7 +2394,7 @@ public class PackageManagerService extends IPackageManager.Stub {
             File frameworkDir = new File(Environment.getRootDirectory(), "framework");
 
             final VersionInfo ver = mSettings.getInternalVersion();
-            mIsUpgrade = !Build.FINGERPRINT.equals(ver.fingerprint);
+            mIsUpgrade = !Build.VELVET.equals(ver.fingerprint);
 
             // when upgrading from pre-M, promote system app permissions from install to runtime
             mPromoteSystemApps =
@@ -2761,7 +2761,7 @@ public class PackageManagerService extends IPackageManager.Stub {
                                         | Installer.FLAG_CLEAR_CODE_CACHE_ONLY);
                     }
                 }
-                ver.fingerprint = Build.FINGERPRINT;
+                ver.fingerprint = Build.VELVET;
             }
 
             checkDefaultBrowser();
@@ -19170,7 +19170,7 @@ Slog.v(TAG, ":: stepped forward, applying functor at tag " + parser.getName());
                     Slog.w(TAG, "Failed to scan " + ps.codePath + ": " + e.getMessage());
                 }
 
-                if (!Build.FINGERPRINT.equals(ver.fingerprint)) {
+                if (!Build.VELVET.equals(ver.fingerprint)) {
                     clearAppDataLIF(ps.pkg, UserHandle.USER_ALL,
                             StorageManager.FLAG_STORAGE_DE | StorageManager.FLAG_STORAGE_CE
                                     | Installer.FLAG_CLEAR_CODE_CACHE_ONLY);
