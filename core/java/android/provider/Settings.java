f9f49bfe77f32dd67692b2266c8517b3fa4e5ac9
Jean-Pierre Rasquin <yank555.lu@gmail.com>
2015-12-24 17:06:09 +0100

    [1/2] Statusbar Network Indicators
    Statusbar: bring back Network indicators [1/2]
    
    source by chameleonos - omnirom - simpleaosp - optipop
    (thanks for sharing your works, guys! )
    
    ezio84: change  attr "min" to "minim" ("min" is already declared)
    Signed-off-by: Jean-Pierre Rasquin <yank555.lu@gmail.com>
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarTransitions.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3636,6 +3636,34 @@ public final class Settings {
          */
         public static final String DOUBLE_TAP_SLEEP_GESTURE = "double_tap_sleep_gesture";
 
+         /**
+         * Change volume up and down handlign based on rotation
+         * @hide
+         */
+        public static final String SWAP_VOLUME_BUTTONS = "swap_volume_buttons";
+
+        /**
+         * Network traffic indicator, goes from least to greatest significant bitwise
+         * 0 = Display up-stream traffic if set
+         * 1 = Display down-stream traffic if set
+         * 2 = Show as Byte/s if set
+         * 16-31 = Refresh interval(ms) min: 250 max: 32750 default: 1000
+         * @hide
+         */
+        public static final String NETWORK_TRAFFIC_STATE = "network_traffic_state";
+
+        /**
+         * Whether or not to hide the network traffic indicator when there is no activity
+         * @hide
+         */
+        public static final String NETWORK_TRAFFIC_AUTOHIDE = "network_traffic_autohide";
+
+        /**
+         * Network traffic inactivity threshold (default is 10 kBs)
+         * @hide
+         */
+        public static final String NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD = "network_traffic_autohide_threshold";
+
         /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
