876ec13439531a72d31dd1cd244f90a9c4f808ac
Andrew Boren <andrew.boren@gmail.com>
2016-11-06 01:16:45 +0100

    Screenshot delay [1/2]
    **DU Edits**
    
    Pulled from Evervolv's Extended powermenu mod back in Lollipop, we've gone ahead and extended
    this function to the QS tile and the vol+power gesture. Tweaked also to use a seekbar instead
    of their number preference which is nice but I like seekbars now :-)
    
    https://github.com/Evervolv/android_frameworks_base/commit/ed3d0763acdf0ac44dbcd0efaf1c472dadf90922
    
    Change-Id: I9c254abb0b70303290877d194e9f36b459002222

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3770,6 +3770,12 @@ public final class Settings {
         public static final String STATUSBAR_BATTERY_CHARGING_IMAGE = "statusbar_battery_charging_image";
 
         /**
+         * Change the delay when a screenshot is taken
+         * @hide
+         */
+        public static final String SCREENSHOT_DELAY = "screenshot_delay";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
