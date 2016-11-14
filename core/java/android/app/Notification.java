58174e8891aeefa624d1f565f4cd1a8fdb7653e1
Ivan Iskandar <iiiiskandar14@gmail.com>
2016-11-14 14:26:56 +0100

    Revert "Remove bool configs"
    This reverts commit a37d2505b933a47687421fe97728937f74d533c0.

--- core/java/android/app/Notification.java
+++ core/java/android/app/Notification.java
@@ -3749,7 +3749,8 @@ public class Notification implements Parcelable
         int resolveIconContrastColor() {
             boolean notificationTint = Settings.Secure.getInt(mContext.getContentResolver(),
                 Settings.Secure.NOTIFICATION_TITLE_TINT, 1) == 1;
-            if (!notificationTint) {
+            if (!notificationTint || !Resources.getSystem().getBoolean(
+                R.bool.config_allowNotificationIconTextTinting)) {
                 return mContext.getColor(R.color.notification_icon_default_color);
             } else {
                 return resolveContrastColor();
@@ -3759,7 +3760,8 @@ public class Notification implements Parcelable
         int resolveContrastColor() {
             boolean notificationTint = Settings.Secure.getInt(mContext.getContentResolver(),
                 Settings.Secure.NOTIFICATION_TITLE_TINT, 1) == 1;
-            if (!notificationTint) {
+            if (!notificationTint || !Resources.getSystem().getBoolean(
+                R.bool.config_allowNotificationIconTextTinting)) {
                 return mContext.getColor(R.color.notification_text_default_color);
             }
 
