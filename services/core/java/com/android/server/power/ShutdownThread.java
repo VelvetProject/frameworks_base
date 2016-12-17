5d566e82e8541231d02e5ce54b93526a3c1d4b6d
Ivan Iskandar <iiiiskandar14@gmail.com>
2016-12-18 00:48:56 +0100

    Fix advance power menu derp
    Pressing power off was showing the advance reboot menu instead of confirmation

--- services/core/java/com/android/server/power/ShutdownThread.java
+++ services/core/java/com/android/server/power/ShutdownThread.java
@@ -187,7 +187,7 @@ public final class ShutdownThread extends Thread {
                             : (mReboot
                                     ? com.android.internal.R.string.reboot_title
                                     : com.android.internal.R.string.power_off));
-            if (!advancedReboot) {
+            if (!advancedReboot || !mReboot || mRebootSafeMode) {
                 confirmDialogBuilder.setMessage(resourceId);
             } else {
                 confirmDialogBuilder
@@ -198,7 +198,7 @@ public final class ShutdownThread extends Thread {
                     new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialog, int which) {
-                            if (advancedReboot) {
+                            if (advancedReboot && mReboot && !mRebootSafeMode) {
                                 boolean softReboot = false;
                                 ListView reasonsList = ((AlertDialog)dialog).getListView();
                                 int selected = reasonsList.getCheckedItemPosition();
